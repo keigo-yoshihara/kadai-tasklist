7cfda6526f0b9bed4f8267dafd38b34aee2ea326
keigo.yoshihara <keigo19991130@icloud.com>
2022-11-30 07:02:40 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   
-  # get 'login', to: 'sessions#new'
-  # post 'login', to: 'sessions#create'
-  # delete 'logout', to: 'sessions#destroy'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   resources :users,only:[:index, :show, :create]
   resources :tasks
