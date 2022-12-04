acc0e0d1389423ff2d6fe2ef040c931e9485a1b1
keigo.yoshihara <keigo19991130@icloud.com>
2022-12-04 08:10:49 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:new, :create, :edit, :update, :destroy]
-  
+  before_action :require_user_logged_in, only: [:create, :edit, :update, :destroy]
   # def index
   #   @pagy,@users = pagy(User.order(id: :desc), items: 25)
   # end
